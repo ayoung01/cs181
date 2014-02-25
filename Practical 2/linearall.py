cd551c4ebbb61d1a14c31fef12eb1815b9bdb0ee
vincentli2010@gmail.com <vincentli2010@gmail.com>
2014-02-24 22:54:42 -0500

    linearall

---
+++ Practical 2/linearall.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 09:12:56 2014
+
+@author: vincentli2010
+"""
+
+
+import numpy as np
+from sklearn import linear_model
+from sklearn import cross_validation
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.linear_model import LassoLarsCV
+
+
+
+class LinearAll:
+    """
+    A repertoire of Linear Variable Selection and Prediction Models
+
+    Parameters
+    ----------
+    n_jobs : int, optional
+        Number of jobs to run in parallel (default 1).
+        If -1 all CPUs are used. This will only provide speedup for
+        n_targets > 1 and sufficient large problems
+    pre_dispatch : int, or string, optional
+        Controls the number of jobs that get dispatched during parallel execution. Reducing this number can be useful to avoid an explosion of memory consumption when more jobs get dispatched than CPUs can process. This parameter can be:
+        None, in which case all the jobs are immediately created and spawned. Use this for lightweight and fast-running jobs, to avoid delays due to on-demand spawning of the jobs
+        An int, giving the exact number of total jobs that are spawned
+        A string, giving an expression as a function of n_jobs, as in ‘2*n_jobs’
+    refit : boolean
+        Refit the best estimator with the entire dataset. If “False”,
+        it is impossible to make predictions using this GridSearchCV
+        instance after fitting.
+    iid : boolean, optional
+        If True, the data is assumed to be identically distributed across
+        the folds, and the score is computed from all samples individually,
+        and not the mean loss across the folds.
+        (If the number of data points is the same across folds, either
+        returns the same thing)
+
+    Attributes
+    ----------
+    ols_train,
+    predictions models before variable selection
+    predictions models after variable selection
+    """
+
+    def __init__ (self, cv=20, scoring = 'mean_squared_error',
+                  n_jobs=-1, refit=False, iid=False):
+        self.__name__ = '__main__'
+        """
+        CAUTION: we changed to __main__ so that parallelization works
+        """
+        self.cv = cv
+        self.scoring = scoring
+        self.n_jobs = n_jobs
+        self.refit = refit
+        self.iid = iid
+
+
+    def predict(self, X, y, param_ridge):
+        """
+
+        Prediction Models.
+
+        OLS, PLS, Ridge
+
+        """
+
+        ##################################
+        ## OLS CV
+        ##################################
+        ols_cv = linear_model.LinearRegression(fit_intercept=True,
+                                                  normalize=False,
+                                                  copy_X=True)
+        rss_ols_cv = cross_validation.cross_val_score(
+                ols_cv, X, y,
+                cv=self.cv, scoring=self.scoring,
+                n_jobs=self.n_jobs)
+        """
+
+
+        self.rss_ols_cv.shape = (cv,)
+        """
+
+        ##################################
+        ## PLS CV
+        ##################################
+        tuned_parameters = [{'n_components': range(1, 20)}]
+        pls = PLSRegression()
+        pls_cv = GridSearchCV(pls, tuned_parameters,
+                                cv=self.cv, scoring=self.scoring,
+                                n_jobs=self.n_jobs,
+                                refit=self.refit, iid=self.iid)
+        pls_cv.fit(X, y)
+
+
+        ##################################
+        ## Ridge CV
+        ##################################
+        tuned_parameters = [{'alpha': param_ridge}]
+        ridge = linear_model.Ridge(alpha = 1)
+        ridge_cv = GridSearchCV(ridge, tuned_parameters,
+                                     cv=self.cv, scoring=self.scoring,
+                                     n_jobs=self.n_jobs,
+                                     refit=self.refit, iid=self.iid)
+        ridge_cv.fit(X, y)
+
+        return (rss_ols_cv, pls_cv, ridge_cv)
+
+    def fit(self, X, y):
+        """
+        Variable Selection and Prediction.
+
+        Variable Selection Model: lasso
+        Prediction Models: see self.predict()
+
+        Parameters
+        ----------
+        X : numpy array or sparse matrix of shape [n_samples,n_features]
+            Training data
+        y : numpy array of shape [n_samples, n_targets]
+            Target values
+
+        Returns
+        -------
+        self : returns an instance of self.
+        """
+
+
+        ##################################
+        ## OLS Train
+        ##################################
+        ols_train = linear_model.LinearRegression(fit_intercept=True,
+                                                  normalize=False,
+                                                  copy_X=True)
+        ols_train.fit(X, y)
+        self.rss_ols_train = np.sum((ols_train.predict(X) - y) ** 2)
+        """
+        fit_intercept=True, center the data
+        copy=True, because centering data invovles X -= X_mean
+
+        CAUTION:
+        normalization=False, otherwise involves taking squares of X, lose precision
+
+        self.rss_ols_train.shape = (1,1)
+        """
+
+        ##################################
+        ## Pre Variable Selection Predictions
+        ##################################
+        print "Computing ... "
+        param_ridge_pre = list(np.arange(1e9,2e9,1e8))
+        self.ols_pre, self.pls_pre, self.ridge_pre = \
+            self.predict(X, y, param_ridge_pre)
+
+        ##################################
+        ## Lasso Variable Selection
+        ##################################
+        self.lasso_cv = LassoLarsCV(fit_intercept=True, normalize=True, precompute='auto',
+                            max_iter=X.shape[1]+1000, max_n_alphas=X.shape[1]+1000,
+                            eps= 2.2204460492503131e-16,copy_X=True,
+                            cv=self.cv, n_jobs=self.n_jobs)
+        self.lasso_cv.fit(X, y)
+        """
+        normalize=True, lasso seems to be able to handle itself
+        """
+
+        self.lasso_refit = linear_model.LassoLars(alpha=self.lasso_cv.alpha_,
+                            fit_intercept=True, normalize=True, precompute='auto',
+                            max_iter=X.shape[1]+1000,
+                            eps=2.2204460492503131e-16, copy_X=True,
+                            fit_path=False)
+        self.lasso_refit.fit(X, y)
+        active = self.lasso_refit.coef_ != 0
+        X_selected = X[:, active[0,:]]
+
+        ##################################
+        ## Post Variable Selection Predictions
+        ##################################
+        param_ridge_post = list(np.arange(1,3,0.1))
+        self.ols_post, self.pls_post, self.ridge_post = \
+            self.predict(X_selected, y, param_ridge_post)
+
+
+        return self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Practical 2/main.py
+++ Practical 2/main.py
@@ -10,10 +10,10 @@ import numpy as np
 
 """
 X_train.shape = (1147, n_feaures)
-8 number_of_screens 
-11 production_budget 
+8 number_of_screens
+11 production_budget
 
-14 review word count 
+14 review word count
 
 55-60 sentiments 0.28247*** 58 -> 0.16029 # number of negative [0] sentences in review
 
@@ -34,19 +34,19 @@ X_train.shape = (1147, n_feaures)
 9 oscar_winning_actors_present
 10 oscar_winning_directors_present
 
-15-36 genres 
-37-49 companies 
-50-54 ratings 
+15-36 genres
+37-49 companies
+50-54 ratings
 observations:
-1. companies sharply divide high and low profile revenues 
-"""    
-import pickle as pickle        
-X_train_1, global_feat_dict, y_train, train_ids = pickle.load(open('features.p', 'rb'))         
-X_train_2 = np.load(open('feat.npy', 'rb'))   
-X_train = np.concatenate((X_train_1, X_train_2), axis=1)     
+1. companies sharply divide high and low profile revenues
+"""
+import pickle as pickle
+X_train_1, global_feat_dict, y_train, train_ids = pickle.load(open('features.p', 'rb'))
+X_train_2 = np.load(open('feat.npy', 'rb'))
+X_train = np.concatenate((X_train_1, X_train_2), axis=1)
 
 ##################################
-## 
+##
 ## IMPUTATION
 ##
 ##################################
@@ -56,7 +56,7 @@ X_train = imp.fit_transform(X_train)
 
 
 ##################################
-## 
+##
 ## TRANSFORMATION
 ##
 ##################################
@@ -66,92 +66,179 @@ y_train = np.log(y_train)
 
 # production_budget trasnform by taking power
 POWER = [0.3]
-for power in POWER: 
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 11][:, np.newaxis] ** power), axis=1)
 
 # number_of_screens
 POWER = [2]
-for power in POWER: 
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 8][:, np.newaxis] ** power), axis=1)
 
 ##################################
-## 
+##
 ## Basis Expansion
 ##
 ##################################
 # production_budget trasnform by taking power
 POWER = [-0.3, 0.1, 0.5, 1.5]
-for power in POWER: 
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 11][:, np.newaxis] ** power), axis=1)
 X_train = np.concatenate((X_train, np.log(X_train[:, 11][:, np.newaxis])), axis=1)
 
 # number_of_screens
 POWER = [-2, -1, 0.1, 0.5, 1.5]
-for power in POWER: 
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 8][:, np.newaxis] ** power), axis=1)
 X_train = np.concatenate((X_train, np.log(X_train[:, 8][:, np.newaxis])), axis=1)
 
-#14 review word count    
-POWER = [-2, -1, 0.1, 0.5, 2, 3] 
-for power in POWER: 
+#14 review word count
+POWER = [-2, -1, 0.1, 0.5, 2, 3]
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 14][:, np.newaxis] ** power), axis=1)
 X_train = np.concatenate((X_train, np.log(X_train[:, 14][:, np.newaxis])), axis=1)
 
 #sentiments: avg, numpos, numneg
 POWER = [-8, -7, -5, -4, -3, -2, -1, -0.1 ,0.1, 0.5, 2, 3, 5, 7, 8, 9]
 for i in [55, 57, 58]:
-    for power in POWER: 
+    for power in POWER:
         X_train = np.concatenate((X_train, (X_train[:, i][:, np.newaxis] + 1) ** power), axis=1)
     X_train = np.concatenate((X_train, np.log(X_train[:, i][:, np.newaxis] + 1)), axis=1)
 
 # running time
-POWER = [-2, -1, 0.1, 0.5, 2] 
-for power in POWER: 
+POWER = [-2, -1, 0.1, 0.5, 2]
+for power in POWER:
     X_train = np.concatenate((X_train, X_train[:, 12][:, np.newaxis] ** power), axis=1)
 X_train = np.concatenate((X_train, np.log(X_train[:, 12][:, np.newaxis])), axis=1)
 
 
 # np_release_dates
-POWER = [-2, -1, 0.1, 0.5, 2, 3] 
-for power in POWER: 
+POWER = [-2, -1, 0.1, 0.5, 2, 3]
+for power in POWER:
     X_train = np.concatenate((X_train, (X_train[:, 61][:, np.newaxis] + 1) ** power), axis=1)
 X_train = np.concatenate((X_train, np.log(X_train[:, 61][:, np.newaxis] + 1)), axis=1)
 
 
 
 ##################################
-## 
+##
 ## Segmentation
 ##
 ##################################
 low = 2
 high = 1000
-MASK = [X_train[:, 8] < low, 
+MASK = [X_train[:, 8] < low,
         np.array([all(x) for x in zip(X_train[:,8]>low, X_train[:,8]<high)]),
-        X_train[:, 8] > high]        
-        
+        X_train[:, 8] > high]
+
 keep = np.arange(X_train.shape[1])
 
+mask = MASK[2]
+X = X_train[mask,:][:,keep]
+y = y_train[mask]
+
+
+
 ##################################
-## 
+##
+## Regression
+##
+##################################
+
+
+import linearall
+models = linearall.LinearAll(cv=20, scoring = 'mean_squared_error',
+                  n_jobs=-1, refit=False, iid=False)
+models.fit(X, y)
+
+##################################
+##
+## Print shit
+##
+##################################
+print "==================================================="
+print "===================== SUMMARY ====================="
+print "==================================================="
+print ("Train OLS:\t%.5f" % (models.rss_ols_train/X.shape[0]))
+
+print "\n============ Pre Variable Selection ==============="
+print "OLS:\t%.5f +- %.5f" % (-np.mean(models.ols_pre), np.std(models.ols_pre))
+
+#print("Grid scores on development set:")
+#for params, mean_score, scores in models.pls_pre..grid_scores_:
+#    print("%0.3f (+/-%0.03f) for %r"
+#          % (mean_score, scores.std() / 2, params))
+for params, mean_score, scores in models.pls_pre.grid_scores_:
+    if mean_score == models.pls_pre.best_score_:
+        print ("PLS\t%.5f +- %.5f for %s" %
+            (-mean_score, scores.std(), params))
+
+#print("Grid scores on development set:")
+#for params, mean_score, scores in models.ridge_pre.grid_scores_:
+#    print("%0.3f (+/-%0.03f) for %r"
+#          % (mean_score, scores.std() / 2, params))
+for params, mean_score, scores in models.ridge_pre.grid_scores_:
+    if mean_score == models.ridge_pre.best_score_:
+        print ("Ridge\t%.5f +- %.5f for %s" %
+            (-mean_score, scores.std(), params))
+
+print "\n============== Variable Selection =================="
+print "Selected:\t%d/%d" % (np.sum(models.lasso_refit.coef_ != 0),X.shape[1])
+
+print "\n============ Post Variable Selection ==============="
+print "OLS:\t%.5f +- %.5f" % (-np.mean(models.ols_post), np.std(models.ols_post))
+
+#print("Grid scores on development set:")
+#for params, mean_score, scores in models.pls_post.grid_scores_:
+#    print("%0.3f (+/-%0.03f) for %r"
+#          % (mean_score, scores.std() / 2, params))
+for params, mean_score, scores in models.pls_post.grid_scores_:
+    if mean_score == models.pls_post.best_score_:
+        print ("PLS\t%.5f +- %.5f for %s" %
+            (-mean_score, scores.std(), params))
+
+#print("Grid scores on development set:")
+#for params, mean_score, scores in models.ridge_post.grid_scores_:
+#    print("%0.3f (+/-%0.03f) for %r"
+#          % (mean_score, scores.std() / 2, params))
+for params, mean_score, scores in models.ridge_post.grid_scores_:
+    if mean_score == models.ridge_post.best_score_:
+        print ("Ridge\t%.5f +- %.5f for %s" %
+            (-mean_score, scores.std(), params))
+
+
+
+
+
+
+
+
+
+
+"""
+mask = MASK[2]
+X_regress = X_train[mask,:][:,keep]
+y_regress = y_train[mask]
+
+##################################
+##
 ## Model Selection
 ##
 ##################################
 
 ### OLS Train Scores
-rss = 0    
-for i in range(len(MASK)):    
+rss = 0
+for i in range(len(MASK)):
     mask = MASK[i]
     X_regress = X_train[mask,:][:,keep]
     y_regress = y_train[mask]
-    
+
     from sklearn import linear_model
     regr = linear_model.LinearRegression(fit_intercept=True, normalize=False)
     regr.fit(X_regress, y_regress)
     print("Average squared residual: %.5f"
       % np.mean((regr.predict(X_regress) - y_regress) ** 2))
     rss += np.sum((regr.predict(X_regress) - y_regress) ** 2)
-    
+
 print("Overall:  %.5f" % (rss/1147))
 
 
@@ -162,7 +249,7 @@ regr_final = linear_model.LinearRegression()
 scores = -1 * cross_validation.cross_val_score(
         regr_final, X_regress, y_regress, cv=20, scoring = 'mean_squared_error')
 #pl.clf()
-#pl.plot(scores)        
+#pl.plot(scores)
 print "OLS: %.5f +- %.5f" % (np.mean(scores), np.std(scores))
 
 
@@ -170,7 +257,9 @@ from sklearn.grid_search import GridSearchCV
 tuned_parameters = [{'n_components': range(1, 20)}]
 from sklearn.cross_decomposition import PLSRegression
 pls = PLSRegression()
-clf = GridSearchCV(pls, tuned_parameters, cv=20, scoring='mean_squared_error')
+clf = GridSearchCV(pls, tuned_parameters, cv=20, scoring='mean_squared_error',
+                   n_jobs = 1)
+
 clf.fit(X_regress, y_regress)
 #print("Grid scores on development set:")
 #for params, mean_score, scores in clf.grid_scores_:
@@ -180,6 +269,7 @@ for params, mean_score, scores in clf.grid_scores_:
     if mean_score == clf.best_score_:
         print ("PLS Best: %.5f +- %.5f for %s" %
             (-mean_score, scores.std(), params))
+
 
 
 alphas = list(np.arange(1e9,2e9,1e8))
@@ -204,33 +294,22 @@ import pylab as pl
 
 print("Computing regularization path using the Lars lasso...")
 t1 = time.time()
-model = LassoLarsCV(cv=20).fit(X_regress, y_regress)
+model = LassoLarsCV(fit_intercept=True, normalize=True, precompute='auto',
+                            max_iter=X_regress.shape[1]+1000, max_n_alphas=X_regress.shape[1]+1000,
+                            eps= 2.2204460492503131e-16,copy_X=True,
+                            cv=20, n_jobs=-1).fit(X_regress, y_regress)
 t_lasso_lars_cv = time.time() - t1
 
 # Display results
 m_log_alphas = -np.log10(model.cv_alphas_)
 
-"""
-pl.figure()
-pl.plot(m_log_alphas, model.cv_mse_path_, ':')
-pl.plot(m_log_alphas, model.cv_mse_path_.mean(axis=-1), 'k',
-        label='Average across the folds', linewidth=2)
-pl.axvline(-np.log10(model.alpha_), linestyle='--', color='k',
-           label='alpha CV')
-pl.legend()
 
-pl.xlabel('-log(alpha)')
-pl.ylabel('Mean square error')
-pl.title('Mean square error on each fold: Lars (train time: %.2fs)'
-         % t_lasso_lars_cv)
-pl.axis('tight')
-pl.show()
-"""
 
-clf = linear_model.LassoLars(alpha=model.alpha_)
-clf.fit(X_regress, y_regress)  
+clf = linear_model.LassoLars(alpha=model.alpha_, fit_path=False)
+clf.fit(X_regress, y_regress)
 active = clf.coef_ != 0
-X_regress= X_regress[:, active]
+X_regress= X_regress[:, active[0,:]]
+
 
 # validation scores for OLS
 from sklearn import cross_validation
@@ -238,7 +317,7 @@ regr_final = linear_model.LinearRegression()
 scores = -1 * cross_validation.cross_val_score(
         regr_final, X_regress, y_regress, cv=20, scoring = 'mean_squared_error')
 #pl.clf()
-#pl.plot(scores)        
+#pl.plot(scores)
 print "OLS: %.5f +- %.5f" % (np.mean(scores), np.std(scores))
 
 
@@ -271,3 +350,4 @@ for params, mean_score, scores in search.grid_scores_:
     if mean_score == search.best_score_:
         print ("Ridge Best: %.5f +- %.5f for %s" %
             (-mean_score, scores.std(), params))
+"""
